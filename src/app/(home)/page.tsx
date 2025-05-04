7d210dc924acce7ca383000f1d6665fa0c4f8554
Carlos Bronze <170220+bronze@users.noreply.github.com>
2025-05-04 13:47:46 -0300

    intro image

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,19 +1,25 @@
-import Link from 'next/link';
+import Link from "next/link";
+import Image from "next/image";
+import JWThompson from "./J_Walter_Thompson.png";
 
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
       <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
-        You can open{' '}
-        <Link
-          href="/docs"
-          className="text-fd-foreground font-semibold underline"
-        >
-          /docs
-        </Link>{' '}
-        and see the documentation.
-      </p>
+      <div className="container grid md:grid-cols-2 items-center gap-4 pb-4 ">
+        <div className="md:order-last">
+          <Image src={JWThompson} alt="Architecture" className="mx-auto w-full max-w-[400px] invert dark:invert-0 lg:mx-0" />
+        </div>
+        <div className="">
+          <p className="text-fd-muted-foreground">
+            You can open{" "}
+            <Link href="/docs" className="text-fd-foreground font-semibold underline">
+              /docs
+            </Link>{" "}
+            and see the documentation.
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
